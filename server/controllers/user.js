adceec0ad4c3867b818703e48c6217660e286694
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2020-11-20 21:09:40 +0100

    Users Registration
    EndPoint para el registro de nuevos usuarios y gestión de errores

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,44 @@ const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user");
 
 function signUp(req, res) {
-    console.log('Endpoint de signUp ejecutado')
+    const user = new User();
+        
+    const { name, lastname, email, password, repeatPassword } = req.body;
+    user.name = name;
+    user.lastname = lastname;
+    user.email = email;
+    user.role = "admin";
+    user.active = false;
+
+    if ( !password || !repeatPassword) {
+        res.status(404).send({ message: "Las contraseñas son obligatorias."});
+    } else {
+        if(password !== repeatPassword) {
+            res.status(404).send({message: "Las contraseñas tienen que ser iguales."})
+        } else {
+            
+            bcrypt.hash(password, null, null, function(err, hash) {
+                if(err) {
+                    res.status(500).send({message: "Error al encriptar la contraseña"})
+                } else {
+                    user.password = hash;
+
+                    user.save((err, userStored) => {
+                        if(err) {
+                            res.status(500).send({message: "Email ya usado."})
+                        } else {
+                            if(!userStored) {
+                                res.status(404).send({message: "Error al crear el usuario."});
+                            } else {
+                                res.status(200).send({user: userStored});
+                            }
+                        }
+
+                    })
+                }
+            });
+        }
+    }
 }
 
 module.exports = {
