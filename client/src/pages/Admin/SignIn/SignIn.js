fc5675b4f675aa6e8fc3c1c3493f3900dbeef50f
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2020-11-21 13:50:03 +0100

    BasicStructure_Scss_SigIn_Tabs

---
+++ client/src/pages/Admin/SignIn/SignIn.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Layout, Tabs } from 'antd';
+import { Redirect } from "react-router-dom";
+import Logo from "../../../assets/img/png/logo-white.png";
+
+import './SignIn.scss';
+
+export default function SignIn() {
+    const { Content } = Layout;
+    const { TabPane } = Tabs;
+
+    return (
+        <Layout className="sign-in">
+            <Content className="sign-in__content">
+                <h1 className="sign-in__content-logo">
+                    <img src={Logo} alt="Jose Manuel Dominguez" />
+                </h1>
+
+                <div className="sign-in__content-tabs">
+                    <Tabs type="card">
+                        <TabPane tab= {<span>Entrar</span>} key="1">
+                            Component LoginForm
+                        </TabPane>
+                        <TabPane tab= {<span>Nuevo Usuario</span>} key="2">
+                            Component RegisterForm
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </Content>
+        </Layout>     
+    );
+}
